505523f7c16125f29ba240fc57b56fbd774282f1
Talentaylor <lsx-123@qq.com>
2019-06-05 16:37:00 +0800

    create CompressImage project

---
+++ CompressImage/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace CompressImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.btnCompress = new System.Windows.Forms.Button();
+            this.lbCompressRatio = new System.Windows.Forms.Label();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(377, 20);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(60, 25);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "浏览";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "选择压缩图片";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(98, 20);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(264, 23);
+            this.textBox.TabIndex = 2;
+            // 
+            // btnCompress
+            // 
+            this.btnCompress.Location = new System.Drawing.Point(377, 74);
+            this.btnCompress.Name = "btnCompress";
+            this.btnCompress.Size = new System.Drawing.Size(60, 25);
+            this.btnCompress.TabIndex = 3;
+            this.btnCompress.Text = "压缩";
+            this.btnCompress.UseVisualStyleBackColor = true;
+            // 
+            // lbCompressRatio
+            // 
+            this.lbCompressRatio.AutoSize = true;
+            this.lbCompressRatio.Location = new System.Drawing.Point(12, 77);
+            this.lbCompressRatio.Name = "lbCompressRatio";
+            this.lbCompressRatio.Size = new System.Drawing.Size(93, 17);
+            this.lbCompressRatio.TabIndex = 4;
+            this.lbCompressRatio.Text = "压缩率（10%）";
+            // 
+            // trackBar
+            // 
+            this.trackBar.Location = new System.Drawing.Point(98, 63);
+            this.trackBar.Maximum = 100;
+            this.trackBar.Minimum = 10;
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(264, 45);
+            this.trackBar.TabIndex = 5;
+            this.trackBar.TickFrequency = 5;
+            this.trackBar.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar.Value = 10;
+            this.trackBar.Scroll += new System.EventHandler(this.trackBar_Scroll);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(98, 124);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(264, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "压缩进度";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(377, 124);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(60, 25);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 172);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.trackBar);
+            this.Controls.Add(this.lbCompressRatio);
+            this.Controls.Add(this.btnCompress);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBrowse);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "图片压缩";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button btnCompress;
+        private System.Windows.Forms.Label lbCompressRatio;
+        private System.Windows.Forms.TrackBar trackBar;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ CompressImage/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CompressImage
+{
+    public partial class Form1 : Form
+    {
+        private string image_file = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void trackBar_Scroll(object sender, EventArgs e)
+        {
+            lbCompressRatio.Text = string.Format("压缩率（{0}%）", trackBar.Value);
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    textBox.Text = dialog.FileName;
+                }
+            }
+        }
+    }
+}
